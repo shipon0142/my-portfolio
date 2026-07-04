5e6e58b1533689b1750b258255677bb21ae05e63
Shipon Sarder <shipon0142@gmail.com>
2026-07-05 00:33:21 +0600

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
                 <a href="#testimonial" class="nav-item nav-link">Review</a>
                 <a href="#contact" class="nav-item nav-link">Contact</a>
             </div>
-            <a href="" class="btn btn-outline-primary d-none d-lg-block">Hire Me</a>
+            <a href="" class="btn btn-outline-primary d-none d-lg-block">Hire M</a>
         </div>
     </nav>
     <!-- Navbar End -->
