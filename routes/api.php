ec2513d5d75726de24acff491d6316d60a3ec6ea
Shipon Sarder <shipon0142@gmail.com>
2026-07-09 07:19:01 +0600

    Fix

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MigrationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
     return $request->user();
 });
+
+Route::post('/system/migrate', MigrationController::class)
+    ->middleware('throttle:6,1');
